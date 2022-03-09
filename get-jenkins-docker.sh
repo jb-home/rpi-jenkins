dbfb017cdf5875192590f055cfc66bfa163e4c59
jb-home <15381432+jb-home@users.noreply.github.com>
2022-03-09 15:44:33 +0100

    Update get-jenkins-docker.sh

--- get-jenkins-docker.sh
+++ get-jenkins-docker.sh
@@ -2,9 +2,9 @@
 
 # get jenkins
 JENKINS_VERSION=2.319.3
-wget --timestamping --no-verbose http://mirrors.jenkins.io/war-stable/$JENKINS_VERSION/jenkins.war
+curl -O http://mirrors.jenkins.io/war-stable/$JENKINS_VERSION/jenkins.war
 
 # get docker binaries
 DOCKER_VERSION=20.10.12
-wget --timestamping --no-verbose https://download.docker.com/linux/static/stable/armhf/docker-$DOCKER_VERSION.tgz
+curl -O https://download.docker.com/linux/static/stable/armhf/docker-$DOCKER_VERSION.tgz
 tar xzvf docker-$DOCKER_VERSION.tgz --strip 1 docker/docker
