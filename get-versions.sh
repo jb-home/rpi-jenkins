3e81de72c5312983fe83b397a12436bd596e276a
jb-home <15381432+jb-home@users.noreply.github.com>
2022-03-25 13:13:33 +0100

    Update get-versions.sh

--- get-versions.sh
+++ get-versions.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-# Outputs versions found in specified container, saves same also
-# in properties file for later use in Jenkinsfile
-
 VERSIONS=version.properties
-
-test -n "$1" && (
-    echo JENKINS_VERSION=\"$(docker exec -t $1 cat /data/war/META-INF/MANIFEST.MF | grep Jenkins-Version | awk '{ print $2 }' | tr -d '\r')\"
-    echo JAVA_VERSION=\"$(docker exec -t $1 java -version | grep version | awk -F\" '{ print $2 }')\"
-    echo DOCKER_VERSION=\"$(docker exec -t $1 docker --version | grep version | awk '{ print $3 }' | tr -d ',')\"
-) | tee $VERSIONS || (
-    echo Give container name as parameter!
-    exit 1
-)
+(
+  echo JENKINS_VERSION=\"$(curl --silent https://updates.jenkins.io/current/latestCore.txt)\"
+) | tee $VERSIONS
